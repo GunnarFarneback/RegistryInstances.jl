02335ee356b5b0d2dbca48b3de6fc1f488e7ef3c
Gunnar Farnebäck <gunnar.farneback@inify.com>
2022-07-20 22:47:09 +0200

    Establish an initial public API.

--- src/RegistryInstances.jl
+++ src/RegistryInstances.jl
@@ -5,12 +5,23 @@ Package providing an external source of Pkg's `RegistryInstance`
 abstraction, thus not relying on Pkg internals.
 
 Exported structs:
-* To be determined.
+* `RegistryInstance`
+* `PkgInfo`
+* `PkgEntry`
 
 Exported functions:
-* To be determined.
+* `reachable_registries`
+* `registry_info`
+* `compat_info`
+* `treehash`
+* `uuids_from_name`
+* `isyanked`
 """
 module RegistryInstances
+
+export RegistryInstance, PkgInfo, PkgEntry
+export reachable_registries, registry_info, compat_info
+export treehash, uuids_from_name, isyanked
 
 # These two functions have been lifted from Pkg's
 # `Registry/Registry.jl` file.
@@ -21,7 +32,7 @@ end
 registry_read_from_tarball() = 
     registry_use_pkg_server() && !(get(ENV, "JULIA_PKG_UNPACK_REGISTRY", "") == "true")
 
-# Initially an exact copy of Pkg's `Registry_registry_instance.jl`.
+# Initially an exact copy of Pkg's `Registry/registry_instance.jl`.
 include("registry_instance.jl")
 
 end
